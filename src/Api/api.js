bae1e386324a74edd03be52901ef40273b696c21
McOriginal <originalmc223@gmail.com>
2026-04-30 22:32:24 +0000

    Correction de baseURL

--- src/Api/api.js
+++ src/Api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://localhost:5001/api',
-  baseURL: '/api',
+  baseURL: '/boutique_banconi/api',
 
   headers: {
     'Content-Type': 'application/json',
